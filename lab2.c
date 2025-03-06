ea744fbeada917f1d52a14b1224cb324d25d94d5
Nico Bykhovsky <nico@bykhovsky.com>
2025-03-06 18:42:17 -0500

    Update lab2.c

--- lab2.c
+++ lab2.c
@@ -87,7 +87,7 @@ char ascii_convert(int modifiers, int keycode0) {
     if (uppercase) {
       l = (char)(1 + keycode0); // Offset for shifted special characters
     } else {
-      l = (char)(5 + keycode0); // Offset for normal special characters
+      l = (char)(keycode0 - 10); // Offset for normal special characters
     }
   }
   else {
